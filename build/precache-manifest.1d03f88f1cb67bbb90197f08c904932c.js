21cad1ab729d573c02c87f505325a91b479957fc
Akop K <akop87@gmail.com>
2019-03-11 04:06:18 -0600

    building

---
+++ build/precache-manifest.1d03f88f1cb67bbb90197f08c904932c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f4806006725a310689f3",
+    "url": "/static/js/main.e60ccf3d.chunk.js"
+  },
+  {
+    "revision": "684630195934f059cae0",
+    "url": "/static/js/2.45a2f2d7.chunk.js"
+  },
+  {
+    "revision": "f4806006725a310689f3",
+    "url": "/static/css/main.68345f78.chunk.css"
+  },
+  {
+    "revision": "292c45a4b608326545f3e8eeb494f44a",
+    "url": "/index.html"
+  }
+];
