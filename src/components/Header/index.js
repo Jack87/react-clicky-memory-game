fbee1db1387de97dda3ae6310d804e6fb2ee613f
Akop K <akop87@gmail.com>
2019-03-11 09:01:42 -0600

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ const Header = props => (
   <div className="header">
     <div className="title">
         <br></br><br></br>
-        <h1 className="">Starcraft 2 Units: Clicky Game</h1>
+        <h1 className="">StarCraft 2 Units: Clicky Game</h1>
         <h2 className="">Click on an image to earn points, but don't click on any more than once or back to zero you go!</h2>
         <a href="#game" class="btn btn-primary js-scroll-trigger">Play</a>
 
