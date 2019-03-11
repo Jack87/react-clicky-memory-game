3ceaaecd4276248874ee802c2575fd39492864ab
Akop K <akop87@gmail.com>
2019-03-11 04:10:52 -0600

    Updates

---
+++ precache-manifest.1566f337b55c56c0a61919c57a40d511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd39fd582f3fa594ec57",
+    "url": "/react-clicky-memory-game/static/js/runtime~main.0b7826eb.js"
+  },
+  {
+    "revision": "054b96ebd8e8fdefe68a",
+    "url": "/react-clicky-memory-game/static/js/main.cb59d164.chunk.js"
+  },
+  {
+    "revision": "684630195934f059cae0",
+    "url": "/react-clicky-memory-game/static/js/2.45a2f2d7.chunk.js"
+  },
+  {
+    "revision": "054b96ebd8e8fdefe68a",
+    "url": "/react-clicky-memory-game/static/css/main.68345f78.chunk.css"
+  },
+  {
+    "revision": "232c6b2d1f450b41255cb64db096b822",
+    "url": "/react-clicky-memory-game/index.html"
+  }
+];
