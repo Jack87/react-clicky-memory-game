5f23930f97c9c4f0d52e02441ef7e235c9e800f5
Akop K <akop87@gmail.com>
2019-03-11 11:00:48 -0600

    Updates

--- precache-manifest.85bf15627115de269423cbac5293cd2f.js
+++ precache-manifest.85bf15627115de269423cbac5293cd2f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-clicky-memory-game/static/js/runtime~main.0b7826eb.js"
   },
   {
-    "revision": "14ed0575bb8a001321f5",
-    "url": "/react-clicky-memory-game/static/js/main.b0a5b635.chunk.js"
+    "revision": "468cc1efba91ec944efc",
+    "url": "/react-clicky-memory-game/static/js/main.e451c4e7.chunk.js"
   },
   {
     "revision": "684630195934f059cae0",
     "url": "/react-clicky-memory-game/static/js/2.45a2f2d7.chunk.js"
   },
   {
-    "revision": "14ed0575bb8a001321f5",
-    "url": "/react-clicky-memory-game/static/css/main.68345f78.chunk.css"
+    "revision": "468cc1efba91ec944efc",
+    "url": "/react-clicky-memory-game/static/css/main.a65340af.chunk.css"
   },
   {
-    "revision": "db8c37cbf2c2b08c16570b12da72ef77",
+    "revision": "1522d70c2cff247d4f7086090d73b40e",
     "url": "/react-clicky-memory-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-clicky-memory-game/precache-manifest.13ef73c55870eb63e7ba8f1710c9e285.js"
+  "/react-clicky-memory-game/precache-manifest.85bf15627115de269423cbac5293cd2f.js"
 );
 
 workbox.clientsClaim();
